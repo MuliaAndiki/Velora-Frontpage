e4c555083ac5bcd38430130137fd624a5bc6f0e6
diki <muliaandiki@gmaiil.com>
2025-09-30 19:35:30 +0700

    feat: fixed: boilerped

--- src/core/providers/auth.provider.tsx
+++ src/core/providers/auth.provider.tsx
@@ -1,13 +1,48 @@
+'use client';
+
 import React from 'react';
-// import { redirect } from 'next/navigation';
-// import { authValidator } from '@/services/auth/auth.store';
+import { useSelector } from 'react-redux';
+import type { RootState } from '@/stores/store';
+import { usePathname, useRouter } from 'next/navigation';
+import { getCookie } from 'cookies-next';
+import { useAppDispatch } from '@/hooks/dispatch/dispatch';
+import { setCurrentUser } from '@/stores/authSlice/authSlice';
+import { APP_SESSION_COOKIE_KEY } from '@/configs/cookies.config';
 
-export default async function Authproviders({ children }: { children: React.ReactNode }) {
-  // const isAuth = await authValidator();
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
+  const dispatch = useAppDispatch();
 
-  // if (isAuth) {
-  //   redirect('/home');
-  // }
+  React.useEffect(() => {
+    if (!currentUser?.user?.token) {
+      const token = getCookie(APP_SESSION_COOKIE_KEY);
+      if (token) {
+        dispatch(setCurrentUser({ user: { token } } as any));
+      }
+    }
+  }, [currentUser, dispatch]);
+
+  React.useEffect(() => {
+    const isAuthPage =
+      pathname?.startsWith('/login') ||
+      pathname?.startsWith('/register') ||
+      pathname?.startsWith('/home');
+
+    const isAuthenticated = Boolean(currentUser?.user?.token);
+
+    if (!isAuthenticated && !isAuthPage) {
+      router.replace('/login');
+      return;
+    }
+
+    if (isAuthenticated && isAuthPage) {
+      // setUp
+      // router.replace('/home');
+      return;
+    }
+  }, [pathname, currentUser, router]);
 
   return <>{children}</>;
 }
