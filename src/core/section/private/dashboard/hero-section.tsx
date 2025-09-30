770bf5563ca51159a9ee25d01e9691c6462395fa
rahmaddayat <rahmad.dayat1204@gmail.com>
2025-09-30 21:04:35 +0700

    feat: penambahan fitur dashboard

--- src/core/section/private/dashboard/hero-section.tsx
+++ src/core/section/private/dashboard/hero-section.tsx
@@ -5,19 +5,19 @@ const DashboardHeroSection = () =>{
                 <div className="flex-1 p-4 flex flex-col">
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
                         <div className="bg-[#d65206] p-6 rounded-lg shadow-md hover:shadow-lg hover:shadow-gray-700 hover:scale-102 transition duration-300">
-                            <h3 className="text-white text-xl font-bold">Title</h3>
+                            <h3 className="text-white text-xl font-bold">Total Balance</h3>
                             <p className="text-gray-300">Rp0</p>
                         </div>
                             <div className="bg-[#d65206] p-6 rounded-lg shadow-md hover:shadow-lg hover:shadow-gray-700 hover:scale-102 transition duration-300">
-                            <h3 className="text-white text-xl font-bold">Title</h3>
+                            <h3 className="text-white text-xl font-bold">Income</h3>
                         <p className="text-gray-300">Rp0</p>
                         </div>
                         <div className="bg-[#d65206] p-6 rounded-lg shadow-md hover:shadow-lg hover:shadow-gray-700 hover:scale-102 transition duration-300">
-                            <h3 className="text-white text-xl font-bold">Title</h3>
+                            <h3 className="text-white text-xl font-bold">Expense</h3>
                             <p className="text-gray-300">Rp0</p>
                         </div>
                         <div className="bg-[#d65206] p-6 rounded-lg shadow-md hover:shadow-lg hover:shadow-gray-700 hover:scale-102 transition duration-300">
-                            <h3 className="text-white text-xl font-bold">Title</h3>
+                            <h3 className="text-white text-xl font-bold">Saving</h3>
                             <p className="text-gray-300">Rp0</p>
                         </div>
                     </div>
